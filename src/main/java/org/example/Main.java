b63801d97f127ab18256a88e37cbf049f79ad559
Nikita Ivanov <resay99@mail.ru>
2023-03-29 15:10:50 +0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Main {
+
+        public static void main(String[] args) {
+
+            int price = 1800; //цена билета
+            int mile = 20; //цена одной мили равна 20
+
+
+            int quantity = (price / mile);
+            // переменная quantity равна резутату деления цены билета на цену одной мили
+            System.out.println(quantity); // вывод значения перменной quantity
+
+
+    }
+}
